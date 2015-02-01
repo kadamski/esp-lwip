81db126f48597186ba42d9fd57e4e660c312241c
Krzysztof Adamski <k@japko.eu>
2015-02-01 12:12:07 +0100

    pbuf: Fix ping.
    That looks strange and I'm not sure why it's needed but without it we
    get an assert:
    
    icmp_input: moving p->payload to ip header failed
    
    when getting ping request.

--- src/core/pbuf.c
+++ src/core/pbuf.c
@@ -600,7 +600,12 @@ pbuf_header(struct pbuf *p, s16_t header_size_increment)
     } else {
       /* cannot expand payload to front (yet!)
        * bail out unsuccesfully */
-      return 1;
+      if (type == PBUF_REF) {
+         /* increase payload pointer */
+        p->payload = (u8_t *)p->payload - header_size_increment;
+      } else {
+        return 1;
+      }
     }
   } else {
     /* Unknown type */
